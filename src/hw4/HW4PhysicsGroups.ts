c7cc29256b04557c25bf16c7418dee180d29606b
Peter Walsh <peter.t.walsh@stonybrook.edu>
2023-02-05 22:39:48 -0500

    started removing enums.

--- src/hw4/HW4PhysicsGroups.ts
+++ src/hw4/HW4PhysicsGroups.ts
@@ -1,17 +1,16 @@
 /**
  * An enum with all of the physics groups for HW4
  */
-export enum HW4PhysicsGroups {
-    // Physics group for the geese 
-    GOOSE = "GOOSE",
+export const HW4PhysicsGroups = {
     // Physics groups for the player and the player's weapon
-    PLAYER = "PLAYER",
-    PLAYER_WEAPON = "WEAPON",
+    PLAYER: "PLAYER",
+    PLAYER_WEAPON: "WEAPON",
     /* 
         Physics groups for the different tilemap layers. Physics groups for tilemaps are
         embedded in the tilemap layer data by a property called "Group". This lets you
         set the physics group for a particular tilemap layer.
     */
-    GROUND = "GROUND",
-    DESTRUCTABLE = "DESTRUCTABLE"
-}
+    GROUND: "GROUND",
+    DESTRUCTABLE: "DESTRUCTABLE"
+    
+} as const;
