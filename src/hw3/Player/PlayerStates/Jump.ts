1dbc3b85f10e5346d299ae4e87e44ba67e7a720f
Peter Walsh <peter.t.walsh@stonybrook.edu>
2023-02-19 22:29:40 -0500

    Fixed jump audio - wasn't playing I don't think?

--- src/hw3/Player/PlayerStates/Jump.ts
+++ src/hw3/Player/PlayerStates/Jump.ts
@@ -12,7 +12,8 @@ export default class Jump extends PlayerState {
         // Give the player a burst of upward momentum
         this.parent.velocity.y = -200;
         console.log("jumping");
-        this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: jumpAudio, loop: false, holdReference: false});
+        // Play the jump sound for the player
+		this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: jumpAudio, loop: false, holdReference: false});
 	}
 
 	public update(deltaT: number): void {
